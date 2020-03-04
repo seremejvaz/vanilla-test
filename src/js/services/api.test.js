52f41cee671d2c646df63b8b41b2e6fbf9ec0e6a
Serena Mejías <serenamejvaz@gmail.com>
2020-03-04 22:59:33 +0100

    Add jest bable & initial api test

--- src/js/services/api.test.js
+++ src/js/services/api.test.js
@@ -1,3 +1,22 @@
-test('add 1 +2 equal 3', () => {
-  expect(1 + 2).toBe(3);
+import { apiService } from './api';
+
+describe('ApiService', () => {
+  let mockApiService = apiService;
+  beforeEach(() => {
+    mockApiService.axios = { get: () => {} };
+  });
+  describe('getMovieDetails', () => {
+    it('should call axios get passing imdbId', () => {
+      const spy = jest.spyOn(mockApiService.axios, 'get');
+      mockApiService.getMovieDetails('test-it');
+      expect(spy).toHaveBeenCalledWith('&i=test-it');
+    });
+  });
+  describe('getMovies', () => {
+    it('should call axios get with a query', () => {
+      const spy = jest.spyOn(mockApiService.axios, 'get');
+      mockApiService.getMovies('query');
+      expect(spy).toHaveBeenCalledWith('&s=query');
+    });
+  });
 });
