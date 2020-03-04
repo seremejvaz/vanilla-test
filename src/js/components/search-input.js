7c7fd0e923b9b4ec14d54336702631baf8e4c949
Serena Mejías <serenamejvaz@gmail.com>
2020-03-05 00:29:13 +0100

    Add input styles

--- src/js/components/search-input.js
+++ src/js/components/search-input.js
@@ -1,9 +1,8 @@
 import { movieService } from '../services/movies';
 import './sorting-button';
-const searchInput = document.getElementById('input');
+const searchInput = document.getElementById('Search-input');
 
 searchInput.onchange = e => {
   const searchString = e.target.value || '';
-
   movieService.getMovies(searchString);
 };
--- src/js/components/sorting-button.js
+++ src/js/components/sorting-button.js
@@ -2,6 +2,8 @@ import { movieService } from '../services/movies';
 
 const sortingButton = document.getElementById('SortingButton');
 
-sortingButton.onclick = () => {
-  movieService.toggleSorting();
-};
+if (sortingButton) {
+  sortingButton.onclick = () => {
+    movieService.toggleSorting();
+  };
+}
