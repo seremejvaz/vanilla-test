726924c49c3b8c853c747bd44b731d0c780b4f92
Serena Mejías <serenamejvaz@gmail.com>
2020-03-05 15:57:58 +0100

    Change no results class name.

--- src/js/components/NoResults.js
+++ src/js/components/NoResults.js
@@ -3,13 +3,13 @@ import { moviesGrid } from '../App';
 export const generateNoresultsContent = () => {
   if (!document.getElementsByClassName('no-result').length) {
     const el = document.createElement('div');
-    el.classList.add('no-results');
+    el.classList.add('NoResults');
     const html = `
       <img 
-        class="no-results img" 
+        class="NoResults img" 
         src="https://cdn.dribbble.com/users/37530/screenshots/2485318/no-results.png"
       />
-      <p class="no-results text">No results</p>
+      <p class="NoResults text">No results</p>
     `;
     el.innerHTML = html;
     moviesGrid.innerHTML = '';
