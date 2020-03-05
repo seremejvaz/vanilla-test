ff12abe87600fd0320c8dec7f077a13e5be6ec02
Serena Mejías <serenamejvaz@gmail.com>
2020-03-05 20:35:30 +0100

    Change API secure url

--- src/js/constant.js
+++ src/js/constant.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://www.omdbapi.com/?apikey=422350ff&';
+export const API_URL = 'https://www.omdbapi.com/?apikey=422350ff&';
 export const NON_AVAILABLE_KEY = 'N/A';
 
 export const IMDb =
