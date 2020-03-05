166883f46effcce10e129aed34d2f9f3a4bb2e08
Serena Mejías <serenamejvaz@gmail.com>
2020-03-05 20:07:44 +0100

    Change webpack watch

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   entry: path.join(__dirname, 'src', 'index'),
-  watch: true,
+  watch: false,
   output: {
     path: path.join(__dirname, 'dist'),
     publicPath: '/dist/',
